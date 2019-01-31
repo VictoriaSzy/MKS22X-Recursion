5a2447af17de6a713e74786de872ab7b6647b3b2
VictoriaSzy <vszymanowski00@stuy.edu>
2019-01-31 17:03:12 -0500

    Corrected parameters given in testing

--- recursion.java
+++ recursion.java
@@ -1,13 +1,20 @@
+import java.util.* ;
+import java.io.* ;
+
 public class recursion{
   public static void main(String[] args) {
+    System.out.println("-----------------------------------------------------------------") ;
     // Testing the square root methods
-    System.out.println("We are testing how the square root function works with Newton's Square Root Approximation!") ;
+    System.out.println("Testing how the square root function works with Newton's Square Root Approximation!") ;
     System.out.println("Let's start testing!") ;
-    System.out.println("What is the square root of 100?  -->  " + sqrt(100)) ; // should return 10
-    System.out.println("1?  -->   " + sqrt(1)) ; // should be 1
-    System.out.println("2?  -->   " + sqrt(2)) ; // should be 1.4142...
-    System.out.println("3?  -->   " + sqrt(3)) ; // should be 1.732...
-    System.out.println("15562?  -->   " + sqrt(15562)) ; // should be 124.7477...
+    System.out.println("What is the square root of 100?  -->  " + sqrt(100,0.00001)) ; // should return 10
+    System.out.println("1?  -->   " + sqrt(1,0.1)) ; // should be 1
+    System.out.println("2?  -->   " + sqrt(2,0.0001)) ; // should be 1.4142...
+    System.out.println("3?  -->   " + sqrt(3,0.00001)) ; // should be 1.732...
+    System.out.println("15562?  -->   " + sqrt(15562,0.01)) ; // should be around 124.7477... (probably near 124.8)
+    System.out.println("-----------------------------------------------------------------") ;
+    // Testing fibbonaci methods
+
   }
   /*Recursively find the sqrt using Newton's approximation
   *tolerance is the allowed percent error the squared answer is away from n.
@@ -35,7 +42,7 @@ public class recursion{
 
   /*As Per classwork*/
   public static ArrayList<Integer> makeAllSums(){
-
+    
   }
 
 }
